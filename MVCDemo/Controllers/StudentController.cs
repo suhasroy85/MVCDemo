ca495535cd768ba77d82b9bff48b21c8a8e744a3
Suhas Roy <suhasroy15@gmail.com>
2021-04-25 13:06:55 +0530

    Add project files.

---
+++ MVCDemo/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var slist = from s in StudentList
+                        orderby s.RollNo
+                        select s;
+            return View(slist);
+        }
+
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            try
+            {
+                StudentList.Add(s);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public static List<Student> StudentList = new List<Student>
+        {
+
+                new Student
+                {
+                    RollNo=101, Name="Suhas", DeptName="IT"
+                },
+                new Student
+                {
+                    RollNo=102, Name="Geet", DeptName="CSE"
+                }
+
+        };
+
+    }
+}
